c50d190ac49a417ff5db995752d3c37f8579a126
ckdgus7 <niceckdgus7@naver.com>
2025-04-13 18:07:32 +0900

    route 및 화면 업데이트
    (cherry picked from commit e0d2d15623049acd9b822b35ffe3327ada55013a)

--- src/layouts/full/FullLayout.vue
+++ src/layouts/full/FullLayout.vue
@@ -1,20 +1,12 @@
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
-import VerticalSidebarVue from './vertical-sidebar/VerticalSidebar.vue';
 import VerticalHeaderVue from './vertical-header/VerticalHeader.vue';
-import Customizer from './customizer/CustomizerPanel.vue';
-import FooterPanel from './footer/FooterPanel.vue';
-import { useCustomizerStore } from '../../stores/customizer';
-const customizer = useCustomizerStore();
+import VerticalSidebarVue from './vertical-sidebar/VerticalSidebar.vue';
 </script>
 
 <template>
   <v-locale-provider>
-    <v-app
-      theme="PurpleTheme"
-      :class="[customizer.fontTheme, customizer.mini_sidebar ? 'mini-sidebar' : '', customizer.inputBg ? 'inputWithbg' : '']"
-    >
-      <Customizer />
+    <v-app theme="PurpleTheme" :class="['Roboto', 'mini-sidebar', 'inputWithbg']">
       <VerticalSidebarVue />
       <VerticalHeaderVue />
 
@@ -22,23 +14,13 @@ const customizer = useCustomizerStore();
         <v-container fluid class="page-wrapper">
           <div>
             <RouterView />
-            <v-btn
-              class="customizer-btn"
-              size="large"
-              icon
-              variant="flat"
-              color="secondary"
-              @click.stop="customizer.SET_CUSTOMIZER_DRAWER(!customizer.Customizer_drawer)"
-            >
-              <SettingsIcon class="icon" />
-            </v-btn>
           </div>
         </v-container>
-        <v-container fluid class="pt-0">
+        <!-- <v-container fluid class="pt-0">
           <div>
             <FooterPanel />
           </div>
-        </v-container>
+        </v-container> -->
       </v-main>
     </v-app>
   </v-locale-provider>
