856581bcf7dd99069b3244cfab3d5dd59c25f362
ckdgus7 <niceckdgus7@gmail.com>
2025-07-31 16:05:28 +0900

    테스트

---
+++ src/_builder/components/custom/CustomModal.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-dialog v-model="visible" :width="props.width" :persistent="props.persistent">
+    <v-card>
+      <v-card-title>
+        {{ props.title }}
+        <v-spacer />
+        <v-btn icon @click="() => (visible = false)">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+      </v-card-title>
+
+      <v-card-text>
+        <slot></slot>
+      </v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script setup lang="ts">
+const props = defineProps<{
+  width?: string;
+  persistent?: boolean;
+  title: string;
+  element?: any;
+}>();
+const visible = defineModel<boolean>('visible');
+</script>
